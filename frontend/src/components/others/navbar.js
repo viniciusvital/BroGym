3e05f9926bc66c4750ae5e63cfa184bf500bf040
Vinicius Vital <vinicius.vital94@hotmail.com>
2022-04-12 22:17:18 -0300

    inicio criação do navbar

--- frontend/src/components/others/navbar.js
+++ frontend/src/components/others/navbar.js
@@ -3,7 +3,25 @@ import '../../css/navbar.css';
 export default function Navbar() {
     return (
         <nav className="nav">
-            menu
+            <span className="nav-logo">BROGYM</span>
+            <ul>
+                <li>Sobre</li>
+                <div className="nav-dropdown">
+                    <a href="#">Instrutores</a>
+                    <a href="#">Aulas</a>
+                    <a href="#">Preços</a>
+                </div>
+                <li>Agenda</li>
+                <li>Contato</li>
+                <li>Loja</li>
+            </ul>
+            <span>Entrar</span>
+            <div className="nav-socials">
+                <a href="#">IG</a>
+                <a href="#">YT</a>
+                <a href="#">FB</a>
+            </div>
+            <div className="Nav-buycart">CART</div>
         </nav>
     );
 }
