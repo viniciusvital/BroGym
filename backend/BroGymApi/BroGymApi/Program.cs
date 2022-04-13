ebc9c7692742744ce8f5f89b79fc7c2ba4be35c9
junioranheu <junioranheu@gmail.com>
2022-04-13 11:23:28 -0300

    ajustes gerais

--- backend/BroGymApi/BroGymApi/Program.cs
+++ backend/BroGymApi/BroGymApi/Program.cs
@@ -116,16 +116,15 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline;
-//if (app.Environment.IsDevelopment())
-//{
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BromGymApi v1");
-    c.DocExpansion(DocExpansion.None); // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/279
-    }
-);
-//}
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BromGymApi v1");
+        c.DocExpansion(DocExpansion.None); // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/279
+    });
+}
 
 // Cors;
 app.UseCors(x => x
